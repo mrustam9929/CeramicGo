24d173f5095dce5391cf6a0895f29bd7f37266f5
Rustam <mrustam9929@gmail.com>
2025-02-26 13:02:40 +0500

    - Баг фикс

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -4,8 +4,12 @@ import os
 from core.settings.base import *
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
-
+ALLOWED_HOSTS = ["ceramicgo.mdev.uz", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://ceramicgo.mdev.uz",
+    "http://localhost",
+    "http://127.0.0.1"
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
 STATIC_URL = '/static/'
